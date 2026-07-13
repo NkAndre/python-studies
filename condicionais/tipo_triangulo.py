30380e149587787b013ffd86a8700661bcf5c3ac
NkAndre <andre270708@gmail.com>
2026-07-13 19:27:03 -0300

    tipo_triangulo(corrigi detalhes, feito

--- condicionais/tipo_triangulo.py
+++ condicionais/tipo_triangulo.py
@@ -1,17 +1,19 @@
-ladoA = int (input('Insira o primeiro lado: '))
-ladoB = int (input('Insira o segundo lado: '))
-ladoC = int (input('Insira o terceiro lado: '))
+ladoA = int(input("Insira o primeiro lado: "))
+ladoB = int(input("Insira o segundo lado: "))
+ladoC = int(input("Insira o terceiro lado: "))
 
+if ladoA < ladoB + ladoC and ladoB < ladoA + ladoC and ladoC < ladoA + ladoB:
 
-if ladoA <ladoB  + ladoC and ladoB < ladoA + ladoC and ladoC<ladoA+ladoB:
-    print('Os lados formam um triângulo:')  
-elif ladoA == ladoB == ladoC:
-    print('Equilátero (três lados iguais)')
-elif ladoA != ladoB and ladoB !=ladoC and ladoA != ladoC:
-     print('Escaleno (três lados diferentes)')
+    print("Os lados formam um triângulo.")
+
+    if ladoA == ladoB == ladoC:
+        print("Equilátero (três lados iguais)")
+
+    elif ladoA == ladoB or ladoA == ladoC or ladoB == ladoC:
+        print("Isósceles (dois lados iguais)")
+
+    else:
+        print("Escaleno (três lados diferentes)")
+
 else:
-     print("Isósceles (dois lados iguais)")
-
-
-
-   
+    print("Os valores não formam um triângulo.")
