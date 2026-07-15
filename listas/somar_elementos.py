357652241cc2da861a47a11aa8323651870358cc
NkAndre <andre270708@gmail.com>
2026-07-15 19:11:24 -0300

    exc somar elementos feito

---
+++ listas/somar_elementos.py
@@ -0,0 +1,10 @@
+numeros = []
+total = 0
+for n in range(5):
+        numero =int  (input("digite o numero "))
+        numeros.append(numero)
+
+        total+=numero
+print(f'Lista: {numeros}')
+print(f'Soma:  {total}')
+
