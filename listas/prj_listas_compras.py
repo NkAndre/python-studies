0ced5b970e046ec52aa2425b7f181d624d491723
NkAndre <andre270708@gmail.com>
2026-07-16 22:26:15 -0300

    correcao no menu

--- listas/prj_listas_compras.py
+++ listas/prj_listas_compras.py
@@ -1,5 +1,6 @@
    #Projeto : Mini Listas de Compras 
 
-menu = (input('1 - Adicionar Produto\n 2 - Remover Produto\n 3 - Buscar Produto\n 4 - Mostrar Lista\n 5 - Sair\n Escolha :  '))
-
-print(menu)
+while  menu!="5":
+    
+    menu = (input('1 - Adicionar Produto\n 2 - Remover Produto\n 3 - Buscar Produto\n 4 - Mostrar Lista\n 5 - Sair\n Escolha :  '))
+ 
