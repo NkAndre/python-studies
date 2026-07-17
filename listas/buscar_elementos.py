5feb367e2fbf77098d24ec343a120c83bab1fd1d
NkAndre <andre270708@gmail.com>
2026-07-16 22:04:49 -0300

    feat: adicionar busca em lista ignorando maiúsculas e minúsculas

--- listas/buscar_elementos.py
+++ listas/buscar_elementos.py
@@ -2,11 +2,11 @@ linguagens = ["Python", "Java", "PHP", "JavaScript", "C#"]
 
 print(f'Lista atual: {linguagens}')
 
+linguagens_minusculas = [l.lower() for l in linguagens]
+buscar = input("Qual Linguagem  Deseja Procurar ?").lower()
+if buscar in linguagens_minusculas:
+    posicao = linguagens_minusculas.index(buscar)
 
-buscar = (input("Qual Linguagem  Deseja Procurar ?"))
-if buscar in linguagens:
-    posicao = linguagens.index(buscar)
-
-    print(f'Linguagem encontrada : {buscar} , está na posição {posicao}.!')
+    print(f'Linguagem encontrada : {linguagens [posicao]} , está na posição {posicao}.!')
 else:
     print('Nao encontrada')
