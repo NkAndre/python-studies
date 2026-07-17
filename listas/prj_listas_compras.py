74f6ec41fe185f930d1c8c9837c3abd74884dd11
NkAndre <andre270708@gmail.com>
2026-07-16 22:23:11 -0300

    menu (lista de compras ) feito

--- listas/prj_listas_compras.py
+++ listas/prj_listas_compras.py
@@ -1,1 +1,5 @@
    #Projeto : Mini Listas de Compras 
+
+menu = (input('1 - Adicionar Produto\n 2 - Remover Produto\n 3 - Buscar Produto\n 4 - Mostrar Lista\n 5 - Sair\n Escolha :  '))
+
+print(menu)
