29c8f7fcd4fb437a162f642a4dd62a04bcd9b3f3
NkAndre <andre270708@gmail.com>
2026-07-16 22:33:35 -0300

    funcao adicionar Produto finalizado

--- listas/prj_listas_compras.py
+++ listas/prj_listas_compras.py
@@ -1,6 +1,19 @@
    #Projeto : Mini Listas de Compras 
+menu = ''
+produtos = []  
 
 while  menu!="5":
     
     menu = (input('1 - Adicionar Produto\n 2 - Remover Produto\n 3 - Buscar Produto\n 4 - Mostrar Lista\n 5 - Sair\n Escolha :  '))
+    if menu == "1":
+   
+        while True:
+
+            produto =  (input("Adicione produto  (ou digite sair para voltar  ):  "))
+            if produto.lower() == 'sair':
+                break
+            produtos.append(produto)
+            print(f'Produtos adicionados : {produtos}')
+
+
  
