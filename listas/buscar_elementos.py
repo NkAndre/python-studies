bf93f30ecefd03173e3c60367f1e32e8b9ccfe8c
NkAndre <andre270708@gmail.com>
2026-07-16 21:52:37 -0300

    exc buscar elementos feito

--- listas/buscar_elementos.py
+++ listas/buscar_elementos.py
@@ -1,1 +1,10 @@
 linguagens = ["Python", "Java", "PHP", "JavaScript", "C#"]
+
+print(f'Lista atual: {linguagens}')
+
+
+buscar = (input("Qual Linguagem  Deseja Procurar ?"))
+if buscar in linguagens:
+    print('Linguagem encontrada!')
+else:
+    print('Nao encontrada')
