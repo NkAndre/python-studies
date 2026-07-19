d34b8ae51b3067b87cc0216d181442257c92477b
NkAndre <andre270708@gmail.com>
2026-07-19 13:32:52 -0300

    ajustes min

--- dicionarios/sistema_cadastro_alunos.py
+++ dicionarios/sistema_cadastro_alunos.py
@@ -3,7 +3,7 @@ alunos = []
 
 while  menu!="5":
     print("\n===== CADASTRO DE ALUNOS =====")
-    menu = (input('1 - Cadastrar Alunos\n 2 - Listar Alunos\n 3 - Buscar aluno\n 4 -  Remover aluno\n 5 - Sair\n Escolha :  '))
+    menu = (input(' 1 - Cadastrar Alunos\n 2 - Listar Alunos\n 3 - Buscar aluno\n 4 - Remover aluno\n 5 - Sair\n Escolha :  '))
 
     if menu =='1':
         while True:
