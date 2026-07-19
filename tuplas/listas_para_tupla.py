4de16b868bb50423c2f79e4f7076af7e5f4249a2
NkAndre <andre270708@gmail.com>
2026-07-18 21:47:56 -0300

    exc conversao de listas pra tupla feito

---
+++ tuplas/listas_para_tupla.py
@@ -0,0 +1,8 @@
+lista = []
+for n in range(5):
+        numero =int  (input("digite o numero "))
+        lista.append(numero)
+
+print(f'lista: {lista}')
+tupla = tuple(lista)
+print(f'tupla : {tupla}')
