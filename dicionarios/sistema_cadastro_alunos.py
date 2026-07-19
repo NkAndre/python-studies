e19af3e577ba2f24cf4a07d8761cda70a76c949d
NkAndre <andre270708@gmail.com>
2026-07-19 12:52:29 -0300

    cadastrar aluno funcionando

--- dicionarios/sistema_cadastro_alunos.py
+++ dicionarios/sistema_cadastro_alunos.py
@@ -1,6 +1,6 @@
 menu = ''
-
 alunos = []
+
 while  menu!="5":
     print("\n===== CADASTRO DE ALUNOS =====")
     menu = (input('1 - Cadastrar Alunos\n 2 - Listar Alunos\n 3 - Buscar aluno\n 4 -  Remover aluno\n 5 - Sair\n Escolha :  '))
@@ -8,14 +8,19 @@ while  menu!="5":
     if menu =='1':
         while True:
            
-           
-
-           if alunos.lower() == 'sair':
+       
+           opcao =  (input("Cadastre Aluno  (ou digite 'sair' para voltar  ):  "))
+           if opcao.lower() == 'sair':
                 break
-                alunos["nome"] = input("Digite o nome: ")
-                alunos["idade"] = int(input("Digite a idade: "))
-                alunos["cidade"] = input("Digite a cidade: ")
+           aluno = {}
+           aluno["nome"] = input("Digite o nome: ")
+           aluno["idade"] = int(input("Digite a idade: "))
+           aluno["cidade"] = input("Digite a cidade: ")
+           alunos.append(aluno)     
+           print(f'Aluno {aluno["nome"]}  Cadastrado com sucesso!!')
+
                 
+            
 
 
     
