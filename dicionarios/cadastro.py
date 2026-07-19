0661f279491fa31c5170c887865c537b6f7102b6
NkAndre <andre270708@gmail.com>
2026-07-18 22:19:46 -0300

    exc cadastro feito

---
+++ dicionarios/cadastro.py
@@ -0,0 +1,6 @@
+dicionario = {}
+dicionario["nome"] = input("Digite o nome: ")
+dicionario["idade"] = int(input("Digite a idade: "))
+dicionario["cidade"] = input("Digite a cidade: ")
+print(dicionario)
+
