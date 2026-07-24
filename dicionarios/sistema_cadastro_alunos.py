d65515ba6e26853624e587ee344f5779fc888eae
NkAndre <andre270708@gmail.com>
2026-07-23 22:08:20 -0300

    troquei a frase do print inicial do menu

--- dicionarios/sistema_cadastro_alunos.py
+++ dicionarios/sistema_cadastro_alunos.py
@@ -2,7 +2,7 @@ menu = ''
 alunos = []
 
 while  menu!="5":
-    print("\n===== CADASTRO DE ALUNOS =====")
+    print("\n===== MENU PRINCIPAL =====")
     menu = (input(' 1 - Cadastrar Alunos\n 2 - Listar Alunos\n 3 - Buscar aluno\n 4 - Remover aluno\n 5 - Sair\n Escolha :  '))
 
     if menu =='1':
